650a4336a21a0b692603f5bafefddb7c3bfa3d39
Genadi Samokovarov <gsamokovarov@gmail.com>
2016-10-07 11:42:25 +0300

    Introduce the tests

--- test/serializr_test.rb
+++ test/serializr_test.rb
@@ -1,11 +1,67 @@
+# frozen-string-literal: true
+
 require 'test_helper'
 
-class SerializrTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Serializr::VERSION
+class SerializrTest < ActiveSupport::TestCase
+  class TestingSerializer < Serializr
+    attributes :foo, :bar
+
+    def bar
+      [object.bar]
+    end
   end
 
-  def test_it_does_something_useful
-    assert false
+  class PlainSerializer < Serializr
+    attributes :foo, :bar, :quux
+  end
+
+  class PlainerSerializer < PlainSerializer
+    attributes :foobar
+
+    def foobar
+      if options[:special]
+        'special'
+      else
+        "#{foo}#{bar}"
+      end
+    end
+  end
+
+  TestObject = Struct.new(:foo, :bar, :quux)
+
+  test 'json serialization' do
+    serializer = TestingSerializer.new(object)
+
+    assert_equal ['bar'], serializer.bar
+    assert_as_json serializer, foo: :foo, bar: [:bar]
+  end
+
+  test 'multiple serializer classes do not mix attributes' do
+    serializer = PlainSerializer.new(object)
+
+    assert_as_json serializer, foo: :foo, bar: :bar, quux: :quux
+  end
+
+  test 'can inherit attributes from other serializers' do
+    serializer = PlainerSerializer.new(object)
+
+    assert_as_json serializer, foo: :foo, bar: :bar, foobar: :foobar, quux: :quux
+  end
+
+  test 'collection class can inherit attributes from other serializers' do
+    serializer = PlainerSerializer[].new([object], special: true)
+
+    assert_as_json serializer, [foo: :foo, bar: :bar, foobar: :special, quux: :quux]
+  end
+
+  test 'collection class can be instantiated on the spot' do
+    collection = [object]
+    serializer = PlainerSerializer[collection, special: true]
+
+    assert_as_json serializer, [foo: :foo, bar: :bar, foobar: :special, quux: :quux]
+  end
+
+  def object
+    TestObject.new('foo', 'bar', 'quux')
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,28 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'serializr'
 
+require 'serializr'
+require 'active_support/all'
 require 'minitest/autorun'
+
+module JsonAssertions
+  def assert_as_json(object, expected)
+    assert_equal normalize_jsonable(expected.as_json), normalize_jsonable(object.as_json)
+  end
+
+  private
+
+  def normalize_jsonable(json)
+    case json
+    when Hash
+      json.deep_stringify_keys
+    when Array
+      json.map { |element| normalize_jsonable(element) }
+    else
+      json
+    end
+  end
+end
+
+class ActiveSupport::TestCase
+  include JsonAssertions
+end

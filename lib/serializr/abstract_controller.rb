2286c9ebdce77b556c3340bb7c97edeee23271eb
Genadi Samokovarov <gsamokovarov@gmail.com>
2016-10-07 14:58:56 +0300

    Automagically infer collections serializers

--- lib/serializr/abstract_controller.rb
+++ lib/serializr/abstract_controller.rb
@@ -10,8 +10,16 @@ class Serializr
       # The resource can be nil, check if it is explicitly passed as such.
       if options.key?(:json)
         resource = options[:json]
-        serializer = options.fetch(:serializer) do
-          serializer_class_cache[resource.class] ||= guess_serializer_for(resource.class)
+        serializer = options[:serializer] || begin
+          if resource.respond_to?(:to_ary)
+            # Assume a collection if the resource responds to #to_ary. The
+            # regular arrays and ActiveRecord::Relations do and will be
+            # represented as arrays, while hashes won't be
+            inferred = serializer_class_cache[resource.to_ary.first.class]
+            inferred[] if inferred
+          else
+            serializer_class_cache[resource.class]
+          end
         end
 
         options[:json] = serializer.new(resource, options) if serializer
@@ -23,14 +31,13 @@ class Serializr
     private
 
     def serializer_class_cache
-      @@_serializer_class_cache ||= {}
-    end
-
-    def guess_serializer_for(resource_class)
-      "#{resource_class}Serializer".safe_constantize or
-        # Try to infer the superclass, if someone let a serializer for it and
-        # not the concrete classes. Don't go deeper, just one level is enough.
-        "#{resource_class.superclass}Serializer".safe_constantize
+      @@_serializer_class_cache ||= Hash.new do |_, cls|
+        "#{cls}Serializer".safe_constantize ||
+          # Try to infer the superclass, if someone let a serializer for it and
+          # not the concrete classes. Don't go deeper, though, just one level
+          # is enough.
+          "#{cls.superclass}Serializer".safe_constantize
+      end
     end
   end
 end

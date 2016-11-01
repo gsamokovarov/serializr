8bdd4fecf7f40939a9bdacac4e270a9a51a0daf8
Genadi Samokovarov <gsamokovarov@gmail.com>
2016-11-01 11:19:45 +0200

    Fix the whitespace during attributes generation

--- lib/generators/serializr/templates/serializr.rb
+++ lib/generators/serializr/templates/serializr.rb
@@ -1,6 +1,6 @@
 <% module_namespacing do -%>
 class <%= class_name %>Serializer < <%= parent_class_name %>
-  <% if attributes.present? -%>
+  <% if attributes.present? %>
   attributes <%= attributes.map { |a| a.name.to_sym.inspect }.join(', ') %>
   <% end %>
 end

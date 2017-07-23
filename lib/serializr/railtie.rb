b57696231bfae3303e0b938df3fcb16bcfadf8dd
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-07-23 12:37:18 +0300

    Introduce automatic integration configuration

--- lib/serializr/railtie.rb
+++ lib/serializr/railtie.rb
@@ -2,15 +2,20 @@
 
 class Serializr
   class Railtie < ::Rails::Railtie
+    config.serializr = config.serializer = ActiveSupport::OrderedOptions.new
+    config.serializr.integration = true
+
     initializer 'serializr.initialize' do
-      require 'serializr/integration'
+      if config.serializr.integration
+        require 'serializr/integration'
 
-      # For Rails 5, automatically include this in the API controllers.
-      if defined?(ActionController::API)
-        ActionController::API.include(::Serializr::Integration)
+        # For Rails 5, automatically include this in the API controllers.
+        if defined?(ActionController::API)
+          ActionController::API.include(::Serializr::Integration)
+        end
+
+        ActionController::Base.include(::Serializr::Integration)
       end
-
-      ActionController::Base.include(::Serializr::Integration)
     end
   end
 end
